61b7c726ee4512915a36b2e97d6bf43bd077cc21
Mirko Kämpf <mirko.kaempf@gmail.com>
2014-08-18 20:32:32 +0200

    Update insertSampleQuestions.sh

--- INGEST/FLUME/bin/insertSampleQuestions.sh
+++ INGEST/FLUME/bin/insertSampleQuestions.sh
@@ -9,6 +9,7 @@ clear
 echo Insert a questions now ... 
  
 curl -X POST -H 'Content-Type: application/json; charset=UTF-8' -d '[{"author":"MK","question":"What time is it?","answer":"The best time for SOLR admin work.","context":"dev"}]' http://training03:9090/
+curl -X POST -H 'Content-Type: application/json; charset=UTF-8' -d '[{"author":"MK","question":"What time is later?","answer":"Still a good time for SOLR admin work.","context":"dev"}]' http://training03:9090/
 
 echo Done.
 
