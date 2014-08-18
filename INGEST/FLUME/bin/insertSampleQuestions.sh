9acf2bbc2d1abc050a5ce52617a2c3ec34619824
Mirko Kämpf <mirko.kaempf@gmail.com>
2014-08-18 17:12:29 +0200

    Update insertSampleQuestions.sh

--- INGEST/FLUME/bin/insertSampleQuestions.sh
+++ INGEST/FLUME/bin/insertSampleQuestions.sh
@@ -8,7 +8,7 @@
 clear
 echo Insert some questions now ... 
  
-curl -X POST -H 'Content-Type: application/json; charset=UTF-8' -d '[{"author":"Knut","question":"What is the current time?","answer":"04:09 AM","context":"dev"}]' http://training03.mtv.cloudera.com:9090/ > response.log
+curl -X POST -H 'Content-Type: application/json; charset=UTF-8' -d '[{"author":"MK","question":"What time is it?","answer":"The best time for SOLR admin work.","context":"dev"}]' http://training03.mtv.cloudera.com:9090/ > response.log
 
 
 cat response.log
