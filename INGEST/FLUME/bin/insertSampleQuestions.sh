06b8ccc2bbecc9bda6c069fb9021dcd49a7afe1e
Mirko Kämpf <mirko.kaempf@gmail.com>
2014-08-19 11:50:41 +0200

    Update insertSampleQuestions.sh

--- INGEST/FLUME/bin/insertSampleQuestions.sh
+++ INGEST/FLUME/bin/insertSampleQuestions.sh
@@ -8,11 +8,11 @@
 clear
 echo Insert a questions now ... 
  
-curl -X POST -H 'Content-Type: application/json; charset=UTF-8' -d '[{"type":"docu","question":"What time is it?","answer":"The best time for SOLR admin work.","context":"dev"}]' http://training03:9090/
-curl -X POST -H 'Content-Type: application/json; charset=UTF-8' -d '[{"type":"src","question":"What time is later?","answer":"Still a good time for SOLR admin work.","context":"dev"}]' http://training03:9090/
-curl -X POST -H 'Content-Type: application/json; charset=UTF-8' -d '[{"type":"src","question":"What time is later?","answer":"Still a good time for SOLR admin work.","context":"test"}]' http://training03:9090/
-curl -X POST -H 'Content-Type: application/json; charset=UTF-8' -d '[{"type":"URL","author":"AB","question":"What time is later?","answer":"Still a good time for SOLR admin work.","context":"da"}]' http://training03:9090/
-curl -X POST -H 'Content-Type: application/json; charset=UTF-8' -d '[{"type":"mail","author":"CD","question":"What time is later?","answer":"Still a good time for SOLR admin work.","context":"admin"}]' http://training03:9090/
+curl -X POST -H 'Content-Type: application/json; charset=UTF-8' -d '[{"type":"docu","question":"What time is it?","answer":"The best time for SOLR admin work.","context":"dev"}]' http://training03:9091/
+curl -X POST -H 'Content-Type: application/json; charset=UTF-8' -d '[{"type":"src","question":"What time is later?","answer":"Still a good time for SOLR admin work.","context":"dev"}]' http://training03:9091/
+curl -X POST -H 'Content-Type: application/json; charset=UTF-8' -d '[{"type":"src","question":"What time is later?","answer":"Still a good time for SOLR admin work.","context":"test"}]' http://training03:9091/
+curl -X POST -H 'Content-Type: application/json; charset=UTF-8' -d '[{"type":"URL","author":"AB","question":"What time is later?","answer":"Still a good time for SOLR admin work.","context":"da"}]' http://training03:9091/
+curl -X POST -H 'Content-Type: application/json; charset=UTF-8' -d '[{"type":"mail","author":"CD","question":"What time is later?","answer":"Still a good time for SOLR admin work.","context":"admin"}]' http://training03:9091/
 
 echo Done.
 
