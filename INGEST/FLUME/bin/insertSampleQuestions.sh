8c6537e3a7316963156b5b5a676e4b16ad3966a5
Mirko Kämpf <mirko.kaempf@gmail.com>
2014-08-19 12:55:45 +0200

    Update insertSampleQuestions.sh

--- INGEST/FLUME/bin/insertSampleQuestions.sh
+++ INGEST/FLUME/bin/insertSampleQuestions.sh
@@ -8,9 +8,9 @@
 clear
 echo Insert a questions now ... 
  
-curl -X POST -H 'Content-Type: application/json; charset=UTF-8' -d '[{"tag":"website","type":"docu","question":"What time is it?","answer":"The best time for SOLR admin work.","context":"dev"}]' http://training03:9091/
-curl -X POST -H 'Content-Type: application/json; charset=UTF-8' -d '[{"tag":"website","type":"src","question":"What time is later?","answer":"Still a good time for SOLR admin work.","context":"dev"}]' http://training03:9091/
-curl -X POST -H 'Content-Type: application/json; charset=UTF-8' -d '[{"tag":"website src","type":"src","question":"What time is later?","answer":"Still a good time for SOLR admin work.","context":"test"}]' http://training03:9091/
+curl -X POST -H 'Content-Type: application/json; charset=UTF-8' -d '[{"tag":"website","type":"docu","author":"XYZ","question":"What time is it?","answer":"The best time for SOLR admin work.","context":"dev"}]' http://training03:9091/
+curl -X POST -H 'Content-Type: application/json; charset=UTF-8' -d '[{"tag":"website","type":"src","author":"MK","question":"What time is later?","answer":"Still a good time for SOLR admin work.","context":"dev"}]' http://training03:9091/
+curl -X POST -H 'Content-Type: application/json; charset=UTF-8' -d '[{"tag":"website src","type":"src","author":"BA","question":"What time is later?","answer":"Still a good time for SOLR admin work.","context":"test"}]' http://training03:9091/
 curl -X POST -H 'Content-Type: application/json; charset=UTF-8' -d '[{"tag":"website url","type":"URL","author":"AB","question":"What time is later?","answer":"Still a good time for SOLR admin work.","context":"da"}]' http://training03:9091/
 curl -X POST -H 'Content-Type: application/json; charset=UTF-8' -d '[{"tag":"website mail","type":"mail","author":"CD","question":"What time is later?","answer":"Still a good time for SOLR admin work.","context":"admin"}]' http://training03:9091/
 
