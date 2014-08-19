810db5c4b69f9212e8336b56afd50ea318825e7d
Mirko Kämpf <mirko.kaempf@gmail.com>
2014-08-19 08:24:20 +0200

    Update insertSampleQuestions.sh

--- INGEST/FLUME/bin/insertSampleQuestions.sh
+++ INGEST/FLUME/bin/insertSampleQuestions.sh
@@ -10,6 +10,9 @@ echo Insert a questions now ...
  
 curl -X POST -H 'Content-Type: application/json; charset=UTF-8' -d '[{"author":"MK","question":"What time is it?","answer":"The best time for SOLR admin work.","context":"dev"}]' http://training03:9090/
 curl -X POST -H 'Content-Type: application/json; charset=UTF-8' -d '[{"author":"MK","question":"What time is later?","answer":"Still a good time for SOLR admin work.","context":"dev"}]' http://training03:9090/
+curl -X POST -H 'Content-Type: application/json; charset=UTF-8' -d '[{"author":"AB","question":"What time is later?","answer":"Still a good time for SOLR admin work.","context":"test"}]' http://training03:9090/
+curl -X POST -H 'Content-Type: application/json; charset=UTF-8' -d '[{"author":"AB","question":"What time is later?","answer":"Still a good time for SOLR admin work.","context":"da"}]' http://training03:9090/
+curl -X POST -H 'Content-Type: application/json; charset=UTF-8' -d '[{"author":"CD","question":"What time is later?","answer":"Still a good time for SOLR admin work.","context":"admin"}]' http://training03:9090/
 
 echo Done.
 
